a71227c6263e4bfaa0ddb8bf27184906f5a092e1
whitenoise13 <amnesiamar13@gmail.com>
2024-08-20 10:37:59 +0900

    fix :: cors error 수정

--- crawlingServer/settings.py
+++ crawlingServer/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
+    'corsheaders',
     'news'
 ]
 
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
 ]
 
 ROOT_URLCONF = 'crawlingServer.urls'
